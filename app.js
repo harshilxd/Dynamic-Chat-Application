f892b2f1b53e56f6a1e479bbe34387ef3a06fe53
harshil201003 <90646149+harshilxd@users.noreply.github.com>
2024-08-16 00:38:03 +0530

    Added user profile, and login page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ mongoose.connect('mongodb://localhost:27017/dynamicChat');
 var app = require('express')();
 
 var http = require('http').Server(app);
+
+var userRoute = require('./routes/userRoute');
+
+app.use('/' , userRoute);
 http.listen(3000, function(){
   console.log('listening on *:3000');
 })
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+
+const registerLoad = async (req, res) => {
+    try {
+        res.render('register');
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const register = async (req, res) => {
+    try {
+        const passwordHash = await bcrypt.hash(req.body.password, 10);
+
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            image: 'images/'+req.file.filename,
+            password: passwordHash
+        });
+
+        await user.save();
+
+        res.render('register', {message: 'User registered successfully!'});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = {
+    registerLoad,
+    register
+}
